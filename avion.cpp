1f3926831b22f209c31214b3453a75568dc16e07
Nathan-Kelo <58146047+Nathan-Kelo@users.noreply.github.com>
2021-12-07 15:13:11 +0100

    NK- updates to the files for thread developement, nothing works yet

--- avion.cpp
+++ avion.cpp
@@ -3,14 +3,67 @@
 
 #define CHANCE_DEFAILLANCE 2	//2 chance sur 100 tout les 100km
 
-const std::string Avion::get_flight_number() {
-	return code_de_vol;
+constexpr sf::Texture planeTexture.loadFromFile("files/airplane.png");
+
+Avion::Avion() {
+	sprite.setTexture(&planeTexture);
+	stop_thread = false;
+	initial_clock_ = high_resolution_clock::now();	
+	t_ = std::thread(update_avion, std::ref(*this), std::ref(stop_thread));
 }
 
-float Avion::get_vitesse() {
-	return vitesse;
+sf::RectangleShape Avion::circle_trajectory() {
+	auto current_clock = high_resolution_clock::now();
+	auto dt = (current_clock - initial_clock_).count() / 1e9;
+	initial_clock_ = current_clock;
+	//Do circles around an aeroprt
+	angle_ -= rotation_speed * static_cast<float>(dt);
+	x_ = (radius_ * cos(angle_)) + x_center;
+	y_ = (radius_ * sin(angle_)) + y_center;
+	return update_sprite();
 }
 
+sf::RectangleShape Avion::line_trajectory() {
+	auto current_clock = high_resolution_clock::now();
+	auto dt = (current_clock - initial_clock_).count() / 1e9;
+	initial_clock_ = current_clock;
+	//Do moving from aeroport 1 to 2
+	return update_sprite();
+}
+
+Avion::~Avion() {
+	stop_thread = true;
+	t_.join();
+}
+
+sf::RectangleShape Avion::update_sprite() {
+	sprite.setPosition(get_x(), get_y());
+	sprite.setRotation(get_angle());
+	return sprite;
+}
+
+
+void update_avion(Avion& avion, bool& stop_thread) {
+	while (!stop_thread) {
+		std::this_thread::sleep_for(1s);
+		if()
+		avion.();
+	}
+}
+
+const std::string Avion::get_flight_number() { return code_de_vol; }
+
+float Avion::get_speed() { return speed_; }
+
+float Avion::get_altitude() { return altitude; }
+
+float Avion::get_x() { return x_; }
+
+float Avion::get_y() { return y_; }
+
+float Avion::get_angle() { return angle_; }
+
+/*
 float Avion::get_niveau_essence() {
 	return niveau_essence;
 }
@@ -35,4 +88,4 @@ std::string Avion::get_aeroport_arrive() {
 
 std::string Avion::get_aeroport_depart() {
 	return aeroport_depart;
-}
+}*/
--- avion.h
+++ avion.h
@@ -1,25 +1,62 @@
+#pragma once 
 #include <string>
 #include <vector>
-#include "Point3D.h"
+#include <iostream>
+#include <ctime>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+using namespace std::chrono_literals;
+
 
 class Avion {
 private:
+	std::thread t_;
+	bool stop_thread;
+	high_resolution_clock::time_point initial_clock_;
+	sf::RectangleShape sprite;
+	
 	const std::string code_de_vol;
-	std::string aeroport_depart;
+	float x_ = 0.;
+	float y_ = 0.;
+	float altitude = 0.;
+	float speed_ = 0.;
+	float rotation_speed = 0.;
+	float angle_ = 0.;
+	/*std::string aeroport_depart;
 	std::string aeroport_arrive;
 	int altitude;
 	float vitesse;
 	float niveau_essence;
 	float consomation_essence;	
-	bool defaillance_avion = false;
+	bool defaillance_avion = false;*/
 public:
-	//std::string get_trajectory();
-	std::string get_aeroport_depart();
-	std::string get_aeroport_arrive();
-	float get_vitesse();
-	float get_niveau_essence();
-	int get_altitude();
+	Avion();
+	/*Draws a cirlce around a designated center
+	*/
+	sf::RectangleShape circle_trajectory();
+	sf::RectangleShape line_trajectory();
+	sf::RectangleShape update_sprite();
+	float get_speed();
+	float get_altitude();
+	float get_angle();
+	float get_x();
+	float get_y();	
 	const std::string get_flight_number();
-	bool emergency_landing();
+	/*std::string get_aeroport_depart();
+	std::string get_aeroport_arrive();
+	
+	float get_niveau_essence();
+	
+	
+	bool emergency_landing();*/
+	
 
 };
